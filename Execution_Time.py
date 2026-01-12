7514c10b5df7f35c0c417e74cc350d46cf647c45
Mike Dinder <dindermike@hotmail.com>
2026-01-12 15:33:14 -0700

    Function profiling

--- Execution_Time.py
+++ Execution_Time.py
@@ -22,3 +22,21 @@ duration = duration.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
 
 print(f"Sum of the function is... {sum}.")
 print(f"The duration of the function is... {duration} milliseconds.")
+
+# Profile a Function
+"""
+Will output something that looks like this...
+         6 function calls in 8.263 seconds
+
+   Ordered by: standard name
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    8.263    8.263 <string>:1(<module>)
+        1    8.263    8.263    8.263    8.263 Execution_Time.py:5(loop_nums)
+        1    0.000    0.000    8.263    8.263 {built-in method builtins.exec}
+        2    0.000    0.000    0.000    0.000 {built-in method time.time}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+"""
+import cProfile
+
+cProfile.run(f'loop_nums({user_input})')
