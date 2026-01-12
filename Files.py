3c7f00b982d7c3230b8061531bb88c82353423b3
Mike Dinder <dindermike@hotmail.com>
2026-01-12 16:40:35 -0700

    More files and list operations

--- Files.py
+++ Files.py
@@ -3,9 +3,16 @@ from os import listdir
 from os.path import isfile, join
 
 # E.g. /home/mike/Python_Practice
+print("List all Files in a Directory.")
 directory = input("Please enter the directory you would like to list: ")
 
 files_list = [f for f in listdir(directory) if isfile(join(directory, f))]
 
 for file in files_list:
-    print(file)
+    print("Filename =", file)
+
+    file_extension = file.split('.')
+
+    print('File Extension = ".' + file_extension[-1] + '"')
+
+print('--------------------------------------------------')
---
+++ Lists.py
@@ -0,0 +1,29 @@
+# List and Tuple operations
+
+# Enter numbers and split into a list and a tuple.
+print("Enter a series of numbers. Your output will be a list and a separate tuple of numbers.")
+numbers = [int(num) for num in input("Enter numbers separated by spaces: ").split()]
+tupled = tuple(numbers)
+
+print("List:", numbers)
+print(type(numbers))
+for x in numbers:
+    print(type(x))
+
+print("Tuple:", tupled)
+print(type(tupled))
+for x in tupled:
+    print(type(x))
+
+print('--------------------------------------------------')
+
+# Show the First and Last elements of a list of names
+print("Show the first and last names of a list of names.")
+
+names = [name for name in input("Enter your names separated by spaces: ").split()]
+
+print("Your List of Names =", names)
+print("The First Name/Element is...", names[0])
+print("The Last Name/Element is...", names[-1])
+
+print('--------------------------------------------------')
