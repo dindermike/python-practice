823e93ca6ecac0b794de8cdd86b9e79a7f42bb64
Mike Dinder <dindermike@hotmail.com>
2026-01-12 15:49:46 -0700

    Get more detailed system info

--- Get_Variables.py
+++ Get_Variables.py
@@ -13,6 +13,29 @@ print(os.environ['NAME'])
 print(os.environ['SHELL'])
 
 print('--------------------------------------------------')
+# Get the current version of Python
+import sys
+print("Current Version of Python is...")
+print(sys.version)
+print(sys.version_info)
+
+print('--------------------------------------------------')
+# Get the current version of Ubuntu
+import platform
+import pprint
+
+# Get all the OS release data
+os_release_info = platform.freedesktop_os_release()
+
+ubuntu_version = os_release_info.get("VERSION")
+ubuntu_codename = os_release_info.get("VERSION_CODENAME")
+pretty_name = os_release_info.get("PRETTY_NAME")
+
+print(f"Pretty Name: {pretty_name}")
+print(f"Ubuntu Version: {ubuntu_version}")
+print(f"Codename: {ubuntu_codename.title()}")
+
+print('--------------------------------------------------')
 
 print("The list of all Variables is...")
 for key, value in os.environ.items():
