8f5621e2dd338d8c131c92403c1f512e5e57e5a9
Mike Dinder <dindermike@hotmail.com>
2026-02-05 18:09:58 -0700

    Finished writing the Generate HTML script with some fine tuning of the Example HTML snippet as well. Generated the cooresponding HTML files as well.

--- Files.py
+++ Files.py
@@ -46,12 +46,12 @@ for file in files_list:
     print('***********************************************')
     print("Filename:", file)
 
-    file_extension = file.split('.')
+    filename_list = file.split('.')
     # stx = statx.statx(file).btime
     # dt_object = datetime.fromtimestamp(stx)
     # formatted_date = dt_object.strftime("%a %b %d %H:%M:%S %Y")
 
-    print('File Extension: ".' + file_extension[-1] + '"')
+    print('File Extension: ".' + filename_list[-1] + '"')
     print('File Last Accessed Time:', time.ctime(path.getatime(file)))
     # print(f"File Created Time: {formatted_date}")
     print('File Changed Time:', time.ctime(path.getctime(file)))
